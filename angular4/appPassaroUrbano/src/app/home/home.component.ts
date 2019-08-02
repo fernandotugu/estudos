1c5c3cc55d39e0282bd7e9ee352a4c8c3d67262a
Fernando Silvério Raimundo Jr <fernando.raimundo@crkdomain.br>
2019-08-02 17:19:53 -0300

    Parte 1 do projeto passaro urbano.

---
+++ angular4/appPassaroUrbano/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import OfertaService from '../ofertas.service'
+import Oferta from '../shared/oferta.model'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers:[OfertaService]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas : Oferta[]
+
+  constructor( private ofertasService : OfertaService) { }
+
+  ngOnInit() {
+    /*this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);*/
+
+    this.ofertasService.getOfertas2().then((ofertas: Oferta[])=>{
+      this.ofertas= ofertas;
+    })
+
+  }
+
+}
